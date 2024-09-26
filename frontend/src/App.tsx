f3e7cf3f04331347ee7966a2e68a1317ae515a12
Fkelv <kelvinfundinjagi@gmail.com>
2024-09-26 15:35:26 +0300

    added dasiy ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
 
   return (
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
-
+      <div>
+        <Button>Click me</Button>
+      </div>
   )
 }
 
